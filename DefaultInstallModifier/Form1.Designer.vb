67fda5712b5a578d280dbf361fd478b675e5fcfe
Meiji01 <44115821+Meiji01@users.noreply.github.com>
2018-11-25 13:24:04 +0800

    Add files via upload

---
+++ DefaultInstallModifier/Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnChange = New System.Windows.Forms.Button()
+        Me.btnDefault = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.txtPath = New System.Windows.Forms.TextBox()
+        Me.btnLocate = New System.Windows.Forms.Button()
+        Me.txtLogs = New System.Windows.Forms.TextBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.txtPath64 = New System.Windows.Forms.TextBox()
+        Me.btnLocate64 = New System.Windows.Forms.Button()
+        Me.btnWinRestore = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tmflash = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'btnChange
+        '
+        Me.btnChange.Location = New System.Drawing.Point(385, 12)
+        Me.btnChange.Name = "btnChange"
+        Me.btnChange.Size = New System.Drawing.Size(92, 41)
+        Me.btnChange.TabIndex = 4
+        Me.btnChange.Text = "Apply"
+        Me.btnChange.UseVisualStyleBackColor = True
+        '
+        'btnDefault
+        '
+        Me.btnDefault.Location = New System.Drawing.Point(386, 59)
+        Me.btnDefault.Name = "btnDefault"
+        Me.btnDefault.Size = New System.Drawing.Size(91, 41)
+        Me.btnDefault.TabIndex = 5
+        Me.btnDefault.Text = "Restore Previous"
+        Me.btnDefault.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(386, 106)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(91, 38)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'txtPath
+        '
+        Me.txtPath.BackColor = System.Drawing.Color.White
+        Me.txtPath.Location = New System.Drawing.Point(21, 21)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.ReadOnly = True
+        Me.txtPath.Size = New System.Drawing.Size(242, 20)
+        Me.txtPath.TabIndex = 0
+        '
+        'btnLocate
+        '
+        Me.btnLocate.Location = New System.Drawing.Point(269, 21)
+        Me.btnLocate.Name = "btnLocate"
+        Me.btnLocate.Size = New System.Drawing.Size(78, 20)
+        Me.btnLocate.TabIndex = 1
+        Me.btnLocate.Text = "Locate (x86)"
+        Me.btnLocate.UseVisualStyleBackColor = True
+        '
+        'txtLogs
+        '
+        Me.txtLogs.BackColor = System.Drawing.Color.Black
+        Me.txtLogs.ForeColor = System.Drawing.Color.Lime
+        Me.txtLogs.Location = New System.Drawing.Point(21, 73)
+        Me.txtLogs.Multiline = True
+        Me.txtLogs.Name = "txtLogs"
+        Me.txtLogs.ReadOnly = True
+        Me.txtLogs.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtLogs.Size = New System.Drawing.Size(326, 126)
+        Me.txtLogs.TabIndex = 8
+        '
+        'txtPath64
+        '
+        Me.txtPath64.BackColor = System.Drawing.Color.White
+        Me.txtPath64.Location = New System.Drawing.Point(21, 47)
+        Me.txtPath64.Name = "txtPath64"
+        Me.txtPath64.ReadOnly = True
+        Me.txtPath64.Size = New System.Drawing.Size(242, 20)
+        Me.txtPath64.TabIndex = 2
+        '
+        'btnLocate64
+        '
+        Me.btnLocate64.Location = New System.Drawing.Point(269, 47)
+        Me.btnLocate64.Name = "btnLocate64"
+        Me.btnLocate64.Size = New System.Drawing.Size(78, 20)
+        Me.btnLocate64.TabIndex = 3
+        Me.btnLocate64.Text = "Locate (x64)"
+        Me.btnLocate64.UseVisualStyleBackColor = True
+        '
+        'btnWinRestore
+        '
+        Me.btnWinRestore.Location = New System.Drawing.Point(385, 152)
+        Me.btnWinRestore.Name = "btnWinRestore"
+        Me.btnWinRestore.Size = New System.Drawing.Size(91, 47)
+        Me.btnWinRestore.TabIndex = 7
+        Me.btnWinRestore.Text = "Restore Windows Default"
+        Me.btnWinRestore.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(330, 209)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(147, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Tool Created by MeijSoft"
+        '
+        'tmflash
+        '
+        Me.tmflash.Interval = 30
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(489, 225)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnWinRestore)
+        Me.Controls.Add(Me.btnLocate64)
+        Me.Controls.Add(Me.txtPath64)
+        Me.Controls.Add(Me.txtLogs)
+        Me.Controls.Add(Me.btnLocate)
+        Me.Controls.Add(Me.txtPath)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDefault)
+        Me.Controls.Add(Me.btnChange)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Opacity = 0.1R
+        Me.Text = "Windows Default Installation Folder Updater"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnChange As System.Windows.Forms.Button
+    Friend WithEvents btnDefault As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents txtPath As System.Windows.Forms.TextBox
+    Friend WithEvents btnLocate As System.Windows.Forms.Button
+    Friend WithEvents txtLogs As System.Windows.Forms.TextBox
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents txtPath64 As System.Windows.Forms.TextBox
+    Friend WithEvents btnLocate64 As System.Windows.Forms.Button
+    Friend WithEvents btnWinRestore As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents tmflash As System.Windows.Forms.Timer
+
+End Class
---
+++ DefaultInstallModifier/Form1.vb
@@ -0,0 +1,170 @@
+﻿Public Class Form1
+    Public currentis64 As Boolean
+
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        txtPath.Text = ""
+        txtPath64.Text = Nothing
+        txtLogs.Text = ""
+    End Sub
+
+    Private Sub btnLocate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLocate.Click
+        FolderBrowserDialog1.ShowDialog()
+        txtPath.Text = FolderBrowserDialog1.SelectedPath.ToString
+    End Sub
+
+    Private Sub btnChange_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnChange.Click
+        Dim proceed As MsgBoxResult
+        proceed = MsgBox("Microsoft does not support changing the location of the Program Files folder. It states that if you change the location of the Program Files folder, you may experience problems with some Microsoft programs or with some software updates. DO YOU WANT TO PROCEED?", vbYesNo, "Warning!")
+        If proceed = vbNo Then
+            GoTo cnld
+            Exit Sub
+        End If
+        Dim oldRegister As String
+        Dim oldRegister2 As String
+        'get previous value
+        txtLogs.AppendText("64 Bit Mode: " & currentis64.ToString & vbCrLf)
+        txtLogs.AppendText("Old registry value: ")
+        oldRegister = My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\", "ProgramFilesDir", Nothing)
+        oldRegister2 = My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\", "ProgramFilesDir (x86)", "unavailable")
+        txtLogs.AppendText(oldRegister & vbCrLf)
+
+
+        'backup old registry for x64
+        txtLogs.AppendText("Backing up old values... " & vbCrLf)
+        If currentis64 = True Then
+            'if no key created yet then enter IF statement, else Skip
+            If My.Computer.Registry.GetValue("HKEY_CURRENT_USER\SOFTWARE\WDIFU\", "ProgBackup(x64)", Nothing) Is Nothing Then
+                My.Computer.Registry.CurrentUser.CreateSubKey("SOFTWARE\WDIFU\")
+                'My.Computer.Registry.SetValue("HKEY_CURRENT_USER\TestKey", "ProgBackup", oldRegister)
+            End If
+            My.Computer.Registry.SetValue("HKEY_CURRENT_USER\SOFTWARE\WDIFU\", "ProgBackup(x64)", oldRegister)
+
+            'backup old registry for x86
+            If My.Computer.Registry.GetValue("HKEY_CURRENT_USER\SOFTWARE\WDIFU\", "ProgBackup(x86)", Nothing) Is Nothing Then
+                My.Computer.Registry.CurrentUser.CreateSubKey("SOFTWARE\WDIFU\")
+                'My.Computer.Registry.SetValue("HKEY_CURRENT_USER\TestKey", "ProgBackup", oldRegister)
+            End If
+            My.Computer.Registry.SetValue("HKEY_CURRENT_USER\SOFTWARE\WDIFU\", "ProgBackup(x86)", oldRegister2)
+        Else
+            'backup old registry for x86
+            If My.Computer.Registry.GetValue("HKEY_CURRENT_USER\SOFTWARE\WDIFU\", "ProgBackup(x86)", Nothing) Is Nothing Then
+                My.Computer.Registry.CurrentUser.CreateSubKey("SOFTWARE\WDIFU\")
+                'My.Computer.Registry.SetValue("HKEY_CURRENT_USER\TestKey", "ProgBackup", oldRegister)
+            End If
+            My.Computer.Registry.SetValue("HKEY_CURRENT_USER\SOFTWARE\WDIFU\", "ProgBackup(x86)", oldRegister)
+        End If
+        txtLogs.AppendText("Backing up old values success..." & vbCrLf)
+
+        Try
+            'update registry using the new value
+            Dim get86value As String
+            Dim get64values As String
+            get86value = txtPath.Text
+            get64values = txtPath64.Text
+            txtLogs.AppendText("Updating values..." & vbCrLf)
+            If currentis64 = True Then
+                'My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion", "ProgramFilesDir (x86)", get86value)
+                My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\WDIFU", "progtest(x86)", get86value)
+                'My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion", "ProgramFilesDir", get64values)
+                My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\WDIFU", "progtest", get64values)
+            Else
+                'My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion", "ProgramFilesDir", get86value)
+                My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\WDIFU", "progtest", get86value)
+            End If
+            txtLogs.AppendText("Update success..." & vbCrLf)
+            Exit Sub
+        Catch ex As Exception
+            txtLogs.AppendText(vbCrLf & "With error: " & ex.Message)
+        End Try
+cnld:
+        MsgBox("Cancelled!", vbOKOnly, "Cancelled!")
+    End Sub
+
+    Function checkbit() As Boolean
+        Dim bitTag As Boolean
+        Try
+            txtLogs.AppendText("Check windows version" & vbCrLf)
+            If My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\", "ProgramFilesDir (x86)", Nothing) Is Nothing Then
+                txtLogs.AppendText("Windows is 32 Bit (IA-32)" & vbCrLf)
+                bitTag = False
+            Else
+                bitTag = True
+                txtLogs.AppendText("Windows is 64 Bit (amd64)" & vbCrLf)
+            End If
+            checkbit = bitTag
+        Catch ex As Exception
+            txtLogs.AppendText(vbCrLf & "Fatal Error: " & ex.Message)
+        End Try
+    End Function
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        tmflash.Enabled = True
+        Debug.Print("Loaded...")
+        Debug.Print("Opacity: " & Me.Opacity)
+        If checkbit() = True Then
+            txtPath64.Enabled = True
+            btnLocate64.Enabled = True
+            currentis64 = True
+            txtPath64.Text = My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\", "ProgramFilesDir", Nothing)
+            txtPath.Text = My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\", "ProgramFilesDir (x86)", "unavailable")
+        Else
+            txtPath64.Enabled = False
+            btnLocate64.Enabled = False
+            currentis64 = False
+            txtPath.Text = My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\", "ProgramFilesDir", Nothing)
+        End If
+    End Sub
+
+    Private Sub btnLocate64_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLocate64.Click
+        FolderBrowserDialog1.ShowDialog()
+        txtPath64.Text = FolderBrowserDialog1.SelectedPath.ToString
+    End Sub
+
+    Private Sub txtPath_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtPath.TextChanged
+        If txtPath.Text <> Nothing Then
+            btnChange.Enabled = True
+        Else
+            btnChange.Enabled = False
+        End If
+    End Sub
+
+    Private Sub txtPath64_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtPath64.TextChanged
+        If txtPath64.Text <> Nothing Then
+            btnChange.Enabled = True
+        Else
+            btnChange.Enabled = False
+        End If
+    End Sub
+
+    Private Sub btnDefault_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDefault.Click
+        txtPath64.Text = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\WDIFU", "ProgBackup(x64)", Nothing)
+        txtPath.Text = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\WDIFU", "ProgBackup(x86)", Nothing)
+    End Sub
+
+    Private Sub btnWinRestore_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnWinRestore.Click
+        If currentis64 = True Then
+            txtPath64.Text = "C:\Program Files"
+            txtPath.Text = "C:\Program Files (x86)"
+            Debug.Print("Windows restored..")
+        Else
+            txtPath64.Text = Nothing
+            txtPath.Text = "C:\Program Files"
+        End If
+        txtLogs.AppendText("Windows Default Loaded" & vbCrLf)
+        MsgBox("Windows default value restored. Dont forget to apply the settings.", vbOKOnly, "MeijSoft")
+    End Sub
+
+    Private Sub tmflash_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmflash.Tick
+        Dim x As Double
+        x = Me.Opacity
+        If Me.Opacity <= 0.99 Then
+            x = x + 0.02
+            Me.Opacity = x
+            Debug.Print("Opacity: " & x)
+        Else
+            '    MsgBox("done")
+            tmflash.Enabled = False
+        End If
+    End Sub
+End Class
